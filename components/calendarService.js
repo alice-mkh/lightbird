92789ac82119fe2c0318a00bb486450fbcad6626
Exalm <exalm7659@gmail.com>
2013-09-20 16:47:57 +0600

    Fixed license headers

--- components/calendarService.js
+++ components/calendarService.js
@@ -1,31 +1,7 @@
 /* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
- * The contents of this file are subject to the Mozilla Public
- * License Version 1.1 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of
- * the License at http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS
- * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
- * implied. See the License for the specific language governing
- * rights and limitations under the License.
- *
- * The Original Code is mozilla.org code.
- *
- * The Initial Developer of the Original Code is Netscape
- * Communications Corporation.  Portions created by Netscape are
- * Copyright (C) 1999 Netscape Communications Corporation.  All
- * Rights Reserved.
- *
- * Contributor(s):
- * Seth Spitzer <sspitzer@netscape.com>
- * Robert Ginda <rginda@netscape.com>
- */
-
-/*
- * This file contains the following calendar related components:
- * 1. Command line handler service, for responding to the -calendar command line
- *    option. (CLineHandler)
- */
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 /* components defined in this file */
 const CLINE_SERVICE_CONTRACTID =
@@ -41,7 +17,7 @@ const STANDARDURL_CONTRACTID =
 const ASS_CONTRACTID =
     "@mozilla.org/appshell/appShellService;1";
 
-/* interafces used in this file */
+/* interfaces used in this file */
 const nsIWindowMediator  = Components.interfaces.nsIWindowMediator;
 const nsICommandLineHandler = Components.interfaces.nsICommandLineHandler;
 const nsICmdLineHandler  = Components.interfaces.nsICmdLineHandler;
